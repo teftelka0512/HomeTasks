c2db703880653ea4228dca0403235184e919606a
Valeria~ <valeriam0593@gmail.com>
2023-01-20 01:06:24 +0300

    Done task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Console.WriteLine("Hello, World!");
+
+// Task4
+
+Console.Write("Введите 1-е число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+int p = Convert.ToInt32(Console.ReadLine()), max = n;
+if (n >= m && n >= p)
+    Console.WriteLine(n);
+else if (m >= n && m >= p)
+    Console.WriteLine(m);
+else
+    Console.WriteLine(p);    
+
+// if (max < m)
+//     max = m;
+// if (max < p)
+//     max = p;
+// Console.WriteLine(max);
