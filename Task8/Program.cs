26ea01c89e09445ed5e98b9b67ac9b5e6fbd9845
Valeria~ <valeriam0593@gmail.com>
2023-01-20 01:22:01 +0300

    Done Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,30 @@
+﻿// Console.WriteLine("Hello, World!");
+
+//Task8
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), count;
+if (n < 0)
+{
+    count = -2;
+    while (count >= n)
+    {
+        Console.Write($"{count} ");
+        count = count - 2;
+    }
+}
+else
+{
+    count = 2;
+     while (count <= n)
+     {
+        Console.Write($"{count} ");
+        count = count + 2;
+     }
+}
+
+
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// for (int i = 2; i <=n; i += 2) 
+//      Console.WriteLine($"{i} ");
