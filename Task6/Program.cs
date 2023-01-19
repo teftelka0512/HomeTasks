fb157ca381fc618cb70b9c8c6798244707122b9d
Valeria~ <valeriam0593@gmail.com>
2023-01-20 01:10:46 +0300

    Done task 6

---
+++ Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿// Console.WriteLine("Hello, World!");
+
+//Task6
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 1)
+    Console.Write("Нечетное");
+else
+    Console.Write("Четное");    
