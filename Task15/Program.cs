5379a18b330d97756e19833f1774096912aa8615
Valeria~ <valeriam0593@gmail.com>
2023-01-25 01:03:17 +0300

    Done Task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,15 @@
+﻿//Console.WriteLine("Hello, World!");
+
+// Task 15
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите день недели: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 6 || n == 7)
+    Console.WriteLine("Выходной день! ");
+else
+    Console.WriteLine("Будний день! ");
