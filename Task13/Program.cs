dde25b350cbf2e010896af1d6b3627465ef35550
Valeria~ <valeriam0593@gmail.com>
2023-01-24 23:13:54 +0300

    done Task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Console.WriteLine("Hello, World!");
+
+// Task 13
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100)
+    Console.WriteLine("Третьей цифры нет!");
+else
+{
+    while ( n >= 999)
+    {
+        n = n / 10;
+    }
+    Console.WriteLine(n);
+    Console.WriteLine(n % 10);
+}
