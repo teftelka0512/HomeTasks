71d20189f4242e622cec894f01b08e8b6e212fc3
Valeria~ <valeriam0593@gmail.com>
2023-01-25 14:11:24 +0300

    Done Task 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -46,7 +46,7 @@ int n1 = n /10000;
 int n2 = (n / 1000) % 10;
 int n4 = (n % 100) / 10;
 int n5 = n % 10;
-if (n1 == n5 || n2 == n4)
+if (n1 == n5 && n2 == n4)
     Console.WriteLine($"Ваше число: {n} - палиндром");
 else
     Console.WriteLine($"Ваше число: {n} - НЕ палиндром");
