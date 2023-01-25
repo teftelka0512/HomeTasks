7273a6dd76ea3dca1d3a2871bfde5a8af5d70d92
Valeria~ <valeriam0593@gmail.com>
2023-01-25 04:21:08 +0300

    Done Task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Console.WriteLine("Hello, World!");
+
+// Task 23
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+    Console.Write($"{Math.Pow(i, 3)} ");    // Console.Write($"{Math.Pow(i, 2)} ");- для квадратов
+
+
+
+
+
