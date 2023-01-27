268785d25073d2f23969e0e500b6081979628666
Valeria~ <valeriam0593@gmail.com>
2023-01-27 23:54:43 +0300

    Done Task 29

---
+++ Task29/Program.cs
@@ -0,0 +1,9 @@
+﻿// Task 29
+
+Console.Write("Введите кол-во элементов массива: ");
+ int n = Convert.ToInt32(Console.ReadLine());
+ int[] array = new int [n];
+ for (int i = 0; i < array.Length; i++)
+      array[i] = i + 1;
+
+ Console.WriteLine($"Результат: [{string.Join(", ", array)}]");     
