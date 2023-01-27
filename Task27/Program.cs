ea44884211fe84fbc8fe2a66a3018823e829e4ff
Valeria~ <valeriam0593@gmail.com>
2023-01-27 23:33:29 +0300

    Done Task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Console.WriteLine("Hello, World!");
+
+// Task 27
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), result = 0;
+while (n > 0)
+{
+    result = result + n % 10;      // result += n % 10;
+    n = n / 10;                    // n /= 10;
+}
+Console.WriteLine(result);
+
+
+
