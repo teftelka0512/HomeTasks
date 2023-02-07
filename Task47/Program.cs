1dd5a94254520d6bfe29cfe18e00ea71e94f824a
Valeria~ <valeriam0593@gmail.com>
2023-02-07 17:43:14 +0300

    Done Task 47

---
+++ Task47/Program.cs
@@ -0,0 +1,291 @@
+﻿
+
+// string[,] table = new string[2,5];
+// // String.Empty
+// // table [0,0] table [0,1] table [0,2] table [0,4]
+// // table [1,0] table [1,1] table [1,2] table [1,4]
+// table[1, 2] = "слово";
+// for (int rows = 0; rows < 2; rows++)
+// {
+//  for (int columns = 0; columns < 5; columns++)
+//  {
+//  Console.WriteLine($"−{table[rows, columns]}−");
+//  }
+// }
+
+// void PrintArray(int[,] matr)
+// {
+//  for (int i = 0; i < matr.GetLength(0); i++)
+//  {
+//  for (int j = 0; j < matr.GetLength(1); j++)
+//  {
+//  Console.Write($"{matr[i, j]} ");
+//  }
+//  Console.WriteLine();
+//  }
+// }
+// void FillArray(int[,] matr)
+// {
+//  for (int i = 0; i < matr.GetLength(0); i++)
+//  {
+//  for (int j = 0; j < matr.GetLength(1); j++)
+//  {
+//  matr[i,j] = new Random().Next(1,10);//[1; 10)
+//  }
+//  }
+// }
+// int[,] matrix = new int[3, 4];
+// PrintArray(matrix);
+// FillArray(matrix);
+// Console.WriteLine();
+// PrintArray(matrix);
+
+
+
+// int[,] pic = new int[,]
+// {
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+// {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// };
+
+// void PrintImage(int[,] image)
+// {
+//  for (int i = 0; i < image.GetLength(0); i++)
+//  {
+//  for (int j = 0; j < image.GetLength(1); j++)
+//  {
+//  if(image[i,j] == 0) Console.Write($" ");
+//  else Console.Write($"+");
+//  }
+//  Console.WriteLine();
+//  }
+// }
+// void FillImage(int row, int col)
+// {
+//  if (pic[row, col] == 0)
+//  {
+//  pic[row, col] = 1;
+//  FillImage(row - 1, col);
+//  FillImage(row, col - 1);
+//  FillImage(row + 1, col);
+//  FillImage(row, col + 1);
+//  }
+// }
+// PrintImage(pic);
+// FillImage(13, 13);
+// PrintImage(pic);
+
+
+
+// double Factorial(int n)
+// {
+//  // 1! = 1
+//  // 0! = 1
+//  if(n == 1) return 1;
+//  else return n * Factorial(n-1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//  Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+
+// double Fibonacci(int n)
+// {
+//  if(n == 1 || n == 2) return 1;
+//  else return Fibonacci(n-1) + Fibonacci(n-2);
+// }
+// for (int i = 1; i < 50; i++)
+// {
+//  Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+// }
+
+
+// Task46        Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+// void InputMatrix(int[,] matrix)
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// matrix[i, j] = new Random().Next(1, 21); // [1, 20]
+// Console.Write($"{matrix[i, j]} \t");
+// }
+// Console.WriteLine();
+// }
+// }
+// Console.Clear();
+// Console.Write("Введите размеры массива: ");
+// int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+// int[,] matrix = new int[size[0], size[1]];
+// InputMatrix(matrix);
+
+
+// Task 48      Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+
+// void InputMatrix(int[,] matrix)
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// matrix[i, j] = i + j;
+// Console.Write($"{matrix[i, j]} \t");
+// }
+// Console.WriteLine();
+// }
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите размеры массива: ");
+// int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+// int[,] matrix = new int[size[0], size[1]];
+// InputMatrix(matrix);
+
+
+// Task 49       Задайте двумерный массив. Найдите элементы, у которых оба индекса не чётные, и замените эти элементы на их квадраты.
+
+// void InputMatrix(int[,] matrix)
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+// }
+// }
+// }
+
+
+// void PrintMatrix(int[,] matrix)
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// Console.Write($"{matrix[i, j]} \t");
+// }
+// Console.WriteLine();
+// }
+// }
+
+
+// void ReleaseMatrix(int[,] matrix)
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// if (i % 2 == 1 && j % 2 == 1)
+// matrix[i, j] *= matrix[i, j];
+// }
+// }
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите размеры массива: ");
+// int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+// int[,] matrix = new int[size[0], size[1]];
+// InputMatrix(matrix);
+// Console.WriteLine("Начальный массив:");
+// PrintMatrix(matrix);
+// ReleaseMatrix(matrix);
+// Console.WriteLine("Конечный массив:");
+// PrintMatrix(matrix);
+
+
+// Task 51      Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+// void InputMatrix(int[,] matrix)
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+// }
+// }
+// }
+
+
+// void PrintMatrix(int[,] matrix)
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// Console.Write($"{matrix[i, j]} \t");
+// }
+// Console.WriteLine();
+// }
+// }
+
+
+// int ReleaseMatrix(int[,] matrix)
+// {
+// int summa = 0;
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// if (i == j)
+// summa += matrix[i, j];
+// }
+// }
+// return summa;
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите размеры массива: ");
+// int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+// int[,] matrix = new int[size[0], size[1]];
+// InputMatrix(matrix);
+// Console.WriteLine("Начальный массив:");
+// PrintMatrix(matrix);
+// Console.WriteLine($"Результат: {ReleaseMatrix(matrix)}");
+
+// Task 47        Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+void InputMatrix(double[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = Math.Round(new Random().NextDouble() * (20 + 20) - 20, 2);
+Console.Write($"{matrix[i, j]} \t");
+}
+Console.WriteLine();
+}
+}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+double[,] matrix = new double[size[0], size[1]];
+InputMatrix(matrix);
